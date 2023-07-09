db5d5fe91420c699340b0f5d9f6d909411458f8c
arryadali <arryadali@gmail.com>
2023-07-09 11:16:09 +0700

    feature home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
+
 import Navbar from './component/navbar';
 import Home from './component/Home';
+import Contact from './component/contact';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Home/>
+      <Contact/>
     </div>
   );
 }
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 const Navbar = () => {
   return (
     <div>
@@ -17,19 +16,24 @@ const Navbar = () => {
 
         <ul class="md:flex md:items-center font-semibold text-[20px] text-white z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top[-400px] transition-all ease-in duration-500">
             <li class="mx-4 my-6 md:my-0">
-                <a href="#" class="hover:text-[#5082FE] duration-500">Home</a>
+                {/* <Link to={"/"}>Home</Link> */}
+                Home
             </li>
             <li class="mx-4 my-6 md:my-0">
-                <a href="#" class="hover:text-[#5082FE] duration-500">About</a>
+                {/* <Link to={"/"}>About</Link> */}
+                About
             </li>
             <li class="mx-4 my-6 md:my-0">
-                <a href="#" class="hover:text-[#5082FE] duration-500">Packages</a>
+                {/* <Link to={"/"}>Packages</Link> */}
+                Packages
             </li>
             <li class="mx-4 my-6 md:my-0">
-                <a href="#" class="hover:text-[#5082FE] duration-500">Places</a>
+                {/* <Link to={"/"}>Places</Link> */}
+                Places
             </li>
             <li class="mx-4 my-6 md:my-0 xl:pr-[94px]">
-                <a href="#" class="hover:text-[#5082FE] duration-500">Contact</a>
+                {/* <Link to={"/"}>Contack</Link> */}
+                Contack
             </li>
 
             <span class="flex">
