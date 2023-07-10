976a041cba90b728c4f3d84b29c73f1b16cc29ad
arryadali <arryadali@gmail.com>
2023-07-10 11:16:34 +0700

    feature contact

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        
+    <div className='bg-[#5082FE] w-full h-auto relative'>
+        <div className='flex flex-col'>
+          <div className='w-[91%] m-auto py-9'>
+            <p className='font-extrabold text-[64px] text-white font-[Poppins]'>We are ready to 
+              <pre className='font-extrabold text-[64px] text-white font-[Poppins]'>provide the best</pre> 
+              <pre className='font-extrabold text-[64px] text-white font-[Poppins]'>trip for you</pre>
+            </p>
+            
+            <div className='w-[17%] mt-6 bg-gradient-to-r from-yellow-300 to-yellow-500 rounded-full flex items-center justify-center p-5 h-16'>
+              <button className='font-medium text-[18px] font-[Poppins]'>Contact Now</button>
+              <span>
+                <i class="uil uil-arrow-circle-right text-3xl items-end"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="../asset/bali.png" alt="" className='absolute right-0 top-1'/>
+        </div>
     </div>
   )
 }
